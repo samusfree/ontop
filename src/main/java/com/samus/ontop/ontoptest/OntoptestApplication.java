0587f939420ff05b2030d800c20bec2156749b5c
Samuel Gonzales <sagonzales89@gmail.com>
2023-05-10 22:39:21 -0500

    initial structure

---
+++ src/main/java/com/samus/ontop/ontoptest/OntoptestApplication.java
@@ -0,0 +1,13 @@
+package com.samus.ontop.ontoptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntoptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntoptestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samus/ontop/ontoptest/OntoptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samus.ontop.ontoptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntoptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
